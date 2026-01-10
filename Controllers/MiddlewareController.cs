8e4c62e1443d8d0072c081371c8ce09bce35ae5a
Hussain <Hussain@HUSSAIN-PC>
2026-01-10 13:55:12 +0200

    Add project files.

---
+++ Controllers/MiddlewareController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UltimateCoreAPITemaplate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MiddlewareController : ControllerBase
+    {
+        // GET: api/<MiddlewareController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            HttpContext context = HttpContext;
+           return Ok(context.Features.);
+        }
+
+       
+    }
+}
